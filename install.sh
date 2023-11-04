27b589ef0be8f55dbb609a6416679d0f756ad0fa
sujay1844 <sujay1844@gmail.com>
2023-11-05 05:21:57 +0530

    Added feedback and keywords

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
-pip install transformers>=4.32.0 optimum>=1.12.0
+pip install transformers>=4.32.0 optimum
 pip install auto-gptq --extra-index-url https://huggingface.github.io/autogptq-index/whl/cu123/
-pip install -q git+https://github.com/huggingface/transformers
-pip install -qU langchain Faiss-gpu tiktoken sentence-transformers
-pip install -qU trl Py7zr auto-gptq optimum
-pip install -q rank_bm25
-pip install -q PyPdf
-pip install cohere openai
-pip install chromadb
+pip install langchain tiktoken sentence-transformers trl Py7zr auto-gptq rank_bm25 PyPdf cohere openai chromadb python-multipart
