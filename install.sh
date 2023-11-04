2d37c3aaa7864fccd99f3455caa5dfc01267b675
sujay1844 <sujay1844@gmail.com>
2023-11-05 00:46:28 +0530

    Changed cuda version

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 pip install transformers>=4.32.0 optimum>=1.12.0
-pip install auto-gptq --extra-index-url https://huggingface.github.io/autogptq-index/whl/cu118/  # Use cu117 if on CUDA 11.7
+pip install auto-gptq --extra-index-url https://huggingface.github.io/autogptq-index/whl/cu123/
 pip install -q git+https://github.com/huggingface/transformers
 pip install -qU langchain Faiss-gpu tiktoken sentence-transformers
 pip install -qU trl Py7zr auto-gptq optimum
