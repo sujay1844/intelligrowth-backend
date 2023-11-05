c829535d5eeb63fff4df4207b0cfa572fa7d717a
sujay1844 <sujay1844@gmail.com>
2023-11-05 08:04:53 +0530

    Added reference properly

--- main.py
+++ main.py
@@ -263,9 +263,7 @@ def generate_keywords(apiBody: APIBody2):
 
     qna = question + "\n" + expected
 
-    reference = vector_db.similarity_search(qna, k=1)
-    reference_content = reference[0]
-    print(reference_content.page_content)
+    reference = vector_db.similarity_search(qna, k=1)[0].page_content
 
     feedback = get_feedback(question, response, expected)
     feedback = re.sub(r'[INST].*[/INST]', '', feedback)
@@ -273,7 +271,7 @@ def generate_keywords(apiBody: APIBody2):
     return {
         "missing_keywords": get_missing_keywords(response,expected),
         "feedback": feedback,
-        "references": "Hello",
+        "references": reference,
     }
 
 @app.get("/clear")
