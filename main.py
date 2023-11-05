446e75b26430f896272a37bfadfdf0dfb2f8739d
sujay1844 <sujay1844@gmail.com>
2023-11-05 07:45:25 +0530

    Changed feedback prompt

--- main.py
+++ main.py
@@ -133,14 +133,15 @@ feedback_generator = pipeline(
 )
 
 keyword_example_prompt = """
-<s>[INST]
+[INST]
 I have the following document:
 - The website mentions that it only takes a couple of days to deliver but I still have not received mine.
 
 Please give me the keywords that are present in this document and separate them with commas.
 Make sure you to only return the keywords and say nothing else. For example, don't say:
 "Here are the keywords present in the document"
-[/INST] meat, beef, eat, eating, emissions, steak, food, health, processed, chicken</s>"""
+meat, beef, eat, eating, emissions, steak, food, health, processed, chicken
+[/INST] """
 
 keyword_ins_prompt = """
 [INST]
